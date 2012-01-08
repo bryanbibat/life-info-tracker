58f089b03b3a83d3d8010448ee800656266bf471
Bryan Bibat <bry@bryanbibat.net>
2012-01-09 04:27:04 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Life Info Tracker
+
+Track Expenses
+
+Accounts - where our money is allocated
+* Name
+* Description
+* Initial Value
+
+Transactions
+* Name
+* Description
+* Amount
+* Source (null if income/interest)
+* Destination (null if expense/fees)
+* Executed At
+* Category Id
+
+Transaction Categories
+* Name
+* Description
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeInfoTracker::Application.config.secret_token = '19a649afd5bc1fba5754e592a0d2cfb13ce53efb5eea4b7378b5956a065953615911d871ab64440236d57c9c45163e7fcfd97cd079d7961114331f4d0a9b7904'
